f3fc67d4b85556baec77dc9edcc2138a943dc0af
Ambroise Palayret <apalayret@oxiane.com>
2024-05-04 10:58:52 +0200

    Modify welcome page

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, isDevMode} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgOptimizedImage, provideImgixLoader} from "@angular/common";
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgOptimizedImage
   ],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  styleUrl: './welcome.component.scss',
+  providers: [
+    provideImgixLoader(isDevMode() ? 'http://localhost:4200/' : 'https://apalayret.github.io/animation-mariage-boubou/'),
+  ],
 })
 export class WelcomeComponent {
 
